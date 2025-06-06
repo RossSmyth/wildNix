f18830a43bc1374215913373b0673095c37980ad
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-06-06 12:34:19 -0400

    Add Wild linker adapter

--- flake.nix
+++ flake.nix
@@ -30,6 +30,29 @@
       };
     in
     {
+      lib.useWildLinker = import ./adapter.nix {
+        inherit pkgs;
+        inherit (pkgs) lib;
+      };
       packages.${system}.default = pkgs.callPackage ./wild.nix { inherit rustPlatform; };
+
+      overlays.default = final: prev: {
+        wild = final.callPackage ./wild.nix { inherit rustPlatform; };
+        useWildLinker = import ./adapter.nix {
+          pkgs = final;
+          lib = final.lib;
+        };
+      };
+
+      checks.${system} = let
+          wildPkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+          };
+        in {
+        adapter = wildPkgs.hello.override (old: {
+          stdenv = wildPkgs.useWildLinker wildPkgs.stdenv;
+        });
+      };
     };
 }
